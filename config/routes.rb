cb8624c96740b4dc2e1442749afff3291c5fc769
kzinti <kzintyempire@gmail.com>
2013-01-03 01:39:26 -0500

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 BootstrapMeBitch::Application.routes.draw do
+  get "users/new"
+
   root to: 'static_pages#home'
 
+  match '/signup', to: 'users#new'
   match '/help', to: 'static_pages#help'
   match '/contact', to: 'static_pages#contact'
   match '/about', to: 'static_pages#about'
